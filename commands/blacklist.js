250d22599b756643f549ed1bc6749cd59ec55c10
Andrew <7147407+ha-noh@users.noreply.github.com>
2021-04-12 23:59:18 -0400

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -2,25 +2,42 @@ const { commandAccessKey } = require('../config.json');
 module.exports = {
 	name: 'blacklist',
 	aliases: ['block', 'stop', 'flag'],
-	description: 'prevent reactions on a specified post or url from triggering a repost - requires a password for use.',
-	usage: '<hall of fame post id || media url> <access key>',
+	description: 'enable or disable reposts for a given url - requires a password for use.',
+	usage: '<media url> <true || false> <access key>',
 	args: true,
 	cooldown: 3,
 	guildOnly: false,
 	execute(message, args, db) {
-		if(args[1] !== commandAccessKey) {
+		if(args[2] !== commandAccessKey) {
 			return message.reply('You didn\'t provide the correct access key!');
 		}
 
-		if(args[0].includes('/')) {
-			return;
+		if(args[1].toLowerCase() == 'true') {
+			updateRow(args[0], 1)
+				.then(() => {
+					message.reply('Url has been blaclisted');
+				})
+				.catch(err => {
+					message.reply('Blacklist operation failed')
+				});
+		}
+		else if(args[1].toLowerCase() == 'false') {
+			updateRow(args[0], 0);
 		}
 		else {
-			return;
+			return message.reply('Your second arg has to be \'true\' or \'false\'');
 		}
 
-		function updateRow(url, msgID, table) {
-			return { url, msgID, table };
+		function updateRow(url, blacklistFlag) {
+			return new Promise((resolve, reject) => {
+				const sql = 'UPDATE posts SET blacklist = ? WHERE url = ?';
+
+				db.run(sql, [blacklistFlag, url], err => {
+					if(err) reject(err.message);
+
+					resolve('row updated');
+				});
+			});
 		}
 	},
 };
